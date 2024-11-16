ab6d5e1a1083d74c6cd6f619962c4894d7ad1e06
DiegoLinares11 <lin221256@uvg.edu.gt>
2024-11-15 19:19:24 -0600

    Ejercicio1

---
+++ ej1.py
@@ -0,0 +1,13 @@
+D = [
+    {'make': 'Nokia', 'model': 216, 'color': 'Black'},
+    {'make': 'Apple', 'model': 2, 'color': 'Silver'},
+    {'make': 'Huawei', 'model': 50, 'color': 'Gold'},
+    {'make': 'Samsung', 'model': 7, 'color': 'Blue'}
+]
+
+# Ordenar la lista de diccionarios por la clave 'model' usando lambda
+D_sorted = sorted(D, key=lambda x: x['model'])
+
+print("Lista ordenada:")
+for item in D_sorted:
+    print(item)
